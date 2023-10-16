bf0e70aafcfcf6f37de821f476563f6aaa50665c
sumonaung23 <sumonaung575@gmail.com>
2023-10-16 15:04:44 +0630

    added posts data

--- src/data.js
+++ src/data.js
@@ -2,29 +2,29 @@ export const posts = [
     {
         id: 1,
         title: "JavaScript Essentials",
-        image: "https://source.unsplash.com/random/?javascriptCode",
+        image: "https://img.freepik.com/free-photo/ai-site-helping-with-software-production_1268-21620.jpg?size=626&ext=jpg&ga=GA1.2.518692104.1697388631&semt=ais",
         desc: "Master the fundamentals of JavaScript to build dynamic web applications.",
-        longDesc: "JavaScript is the cornerstone of modern web development. This course will teach you the core concepts, from variables and functions to asynchronous programming. By the end, you'll be equipped to create interactive web applications with ease."
+        longDesc: "JavaScript is the cornerstone of modern web development. This comprehensive course covers the core concepts, including variables, functions, object-oriented programming, and asynchronous programming. You'll also learn about popular JavaScript libraries and frameworks, such as jQuery and Node.js. By the end, you'll be well-equipped to create interactive and responsive web applications."
     },
     {
     id: 2,
     title: "React Web Development",
-    image: "https://source.unsplash.com/random/?Code",
+    image: "https://img.freepik.com/free-vector/coding_53876-25854.jpg?size=626&ext=jpg&ga=GA1.1.518692104.1697388631&semt=ais",
     desc: "Learn how to build powerful web apps with React, the popular JavaScript library.",
-    longDesc: "React is a game-changer in web development. Dive into the world of component-based UI development and harness the power of state management. By the end of this course, you'll be ready to create modern, efficient web applications using React."
+    longDesc: "React Web Development is your guide to mastering the React library and its ecosystem. You'll delve into component-based UI development, state management with Redux, and routing with React Router. The course also covers best practices for building efficient and scalable web applications using React. By the end, you'll be ready to create modern, high-performance web apps with React."
     },
     {
     id: 3,
     title: "JavaScript for Front-End",
-    image: "https://source.unsplash.com/random/?code",
+    image: "https://img.freepik.com/premium-photo/desktop-source-code-wallpaper-by-computer-language-with-coding-programming_33771-596.jpg?size=626&ext=jpg&ga=GA1.2.518692104.1697388631&semt=ais",
     desc: "Become a front-end developer by mastering JavaScript and its integration with HTML and CSS.",
-    longDesc: "Front-end development is all about creating engaging user interfaces. This course will guide you through the integration of JavaScript, HTML, and CSS, helping you create visually appealing and interactive web pages that delight users."
-    },
+    longDesc: "JavaScript for Front-End is the perfect course for aspiring front-end developers. You'll learn how to use JavaScript to enhance the interactivity and functionality of web pages. This course covers topics such as DOM manipulation, event handling, and asynchronous requests with AJAX. You'll also gain insights into best practices for front-end development, including responsive design and performance optimization."
+    }/* ,
     {
     id: 4,
     title: "React Native Mobile Apps",
     image: "https://source.unsplash.com/random/React.js",
     desc: "Build cross-platform mobile apps with React Native, using your JavaScript skills.",
     longDesc: "React Native allows you to leverage your JavaScript expertise to create mobile apps for both iOS and Android. Learn how to develop native mobile experiences without the need to learn platform-specific languages."
-    },
+    }, */
 ]
