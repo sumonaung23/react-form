18031e357c08eb435763751c8a26142aa1d77ec0
sumonaung23 <sumonaung575@gmail.com>
2023-10-16 15:05:08 +0630

    created and structured navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <span className='logo'>Epiphany</span>
+            <span className='logo'>Front-end Dev</span>
             <ul className="list">
                 <li className="listItem">
-                    <img src="https://source.unsplash.com/random/?flowers" alt="" className="avatar" />
+                    <img src="https://img.freepik.com/free-photo/business-finance-employment-female-successful-entrepreneurs-concept-smiling-professional-businesswoman-real-estate-broker-showing-clients-good-deal-carry-laptop-hand_1258-59121.jpg?size=626&ext=jpg&ga=GA1.1.518692104.1697388631&semt=ais" alt="woman" className="avatar" />
                 </li>
                 <li className="listItem">Susie</li>
                 <li className="listItem">Sign out</li>
