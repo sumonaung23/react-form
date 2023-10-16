83bc48ffe5f3a15d9c0dc3099dff8a70105515b0
sumonaung23 <sumonaung575@gmail.com>
2023-10-16 15:06:12 +0630

    created and structured post section

---
+++ src/pages/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { posts } from '../data'
+
+const Post = () => {
+    const post = posts[2]
+
+    return (
+        <div className='post'>
+            <img src={post.image} alt="" className="postImg" />
+            <h1 className="postTitle">{post.title}</h1>
+            <p className="postDesc">{post.desc}</p>
+            <p className="postLongDesc">{post.longDesc}</p>
+        </div>
+    )
+}
+
+export default Post
