e370329c066ca0bddb3fb08558b2fcaa4ebfe5b1
sumonaung23 <sumonaung575@gmail.com>
2023-10-16 17:42:43 +0630

    added react router and modified with them

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import './App.css';
+import Post from './pages/Post';
+import Login from './pages/Login';
+import { BrowserRouter,Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
+    const user = true;
+
     return (
-        <div className='app'>
-            <Navbar />
-            <Home />
-        </div>
+        <BrowserRouter>
+            <div className='app'>
+                <Navbar user={user} />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={user ? <Navigate to="/" /> : <Login />} />
+                    <Route path='/post/:id' element={user ? <Post /> : <Navigate to="/login" />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
+        
 }
 
 export default App
